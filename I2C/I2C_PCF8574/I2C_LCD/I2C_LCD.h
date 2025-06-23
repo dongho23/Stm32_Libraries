239966a1c8419cecd24e45e68f093b68b70acbb7
mede-erik <mede.erik@gmail.com>
2025-06-23 16:51:57 +0200

    Rimuovi le direttive condizionali per l'inclusione delle librerie HAL in vari file header

--- I2C/I2C_PCF8574/I2C_LCD/I2C_LCD.h
+++ I2C/I2C_PCF8574/I2C_LCD/I2C_LCD.h
@@ -7,36 +7,8 @@
  * @link https://github.com/mede-erik/Stm32_Libraries
  */
 
-#ifdef STM32F0
-#include "stm32f0xx_hal.h" /* Import HAL library */
-#elif defined(STM32F1)
-#include "stm32f1xx_hal.h" /* Import HAL library */
-#elif defined(STM32F2)
-#include "stm32f2xx_hal.h" /* Import HAL library */
-#elif defined(STM32F3)
-#include "stm32f3xx_hal.h" /* Import HAL library */
-#elif defined(STM32F4)
-#include "stm32f4xx_hal.h" /* Import HAL library */
-#elif defined(STM32F7)
-#include "stm32f7xx_hal.h" /* Import HAL library */
-#elif defined(STM32G0)
-#include "stm32g0xx_hal.h" /* Import HAL library */
-#elif defined(STM32G4)
-#include "stm32g4xx_hal.h" /* Import HAL library */
-#elif defined(STM32H7)
-#include "stm32h7xx_hal.h" /* Import HAL library */
-#elif defined(STM32L0)
-#include "stm32l0xx_hal.h" /* Import HAL library */
-#elif defined(STM32L1)
-#include "stm32l1xx_hal.h" /* Import HAL library */
-#elif defined(STM32L5)
-#include "stm32l5xx_hal.h" /* Import HAL library */
-#elif defined(STM32L4)
-#include "stm32l4xx_hal.h" /* Import HAL library */
-#elif defined(STM32H7)
-#include "stm32h7xx_hal.h" /* Import HAL library */
-#else
-#endif
+#ifndef I2C_LCD_H
+#define I2C_LCD_H   
 
 #include "main.h"
 
@@ -60,3 +32,4 @@ void lcd16x2_i2c_display(bool state);
 void lcd16x2_i2c_shiftRight(uint8_t offset);
 void lcd16x2_i2c_shiftLeft(uint8_t offset);
 void lcd16x2_i2c_printf(const char *str, ...);
+
